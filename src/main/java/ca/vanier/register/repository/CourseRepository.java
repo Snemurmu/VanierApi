ba1b247a6adc1ad6f7a8de3cfce67ece1cb155fd
snemurmu <snemurmu@gmail.com>
2023-02-15 00:18:54 -0500

    Fixed issue with the Course repository

--- src/main/java/ca/vanier/register/repository/CourseRepository.java
+++ src/main/java/ca/vanier/register/repository/CourseRepository.java
@@ -1,9 +1,11 @@
 package ca.vanier.register.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import ca.vanier.register.entity.Course;
 
+@Repository
 public interface CourseRepository extends CrudRepository<Course, Long> {
     
 }
